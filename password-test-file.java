743a03bf9e6445d9609183ae97f52e1ca67baa77
Repo Exposed <39785423+repoexposed@users.noreply.github.com>
2018-06-03 20:51:10 -0400

    test-password-file
    Created sample password file to parse.

---
+++ password-test-file.java
@@ -0,0 +1,14 @@
+package com.lc.tools;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Common {
+
+	
+	 static final String DRIVER="com.mysql.jdbc.Driver";
+	 static final String URL="jdbc:mysql://xxx.xxx.xxx.xxx:xxxxx/xxxxxx";
+	 static final String USERNAME="root";
+	 static final String PASSWORD="xxxxxxxxx";	 
+	 
+}
